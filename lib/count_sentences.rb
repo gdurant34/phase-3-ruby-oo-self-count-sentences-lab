a4ecc8b4018cb4664e4c03ba67159af76b5a70d4
Grace Durant <gdurant34@gmail.com>
2022-11-03 18:15:34 -0700

    commit tree 7cca1c49b48cf253d0e58738912fa8fe2098e3f7

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[.?!]/).filter { |sentence| !sentence.empty? }.length
+    # puts self
+    
   end
 end
+
+"This is my string. For real. It is mine. Back off you!".count_sentences
